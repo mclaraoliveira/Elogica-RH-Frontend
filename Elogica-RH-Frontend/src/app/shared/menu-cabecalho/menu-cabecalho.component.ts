8d4e876350dd7f203a2b2b328d59bc0e11813626
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-25 17:04:40 -0300

    Buid: Criar Menu lateral component inicial.

---
+++ Elogica-RH-Frontend/src/app/shared/menu-cabecalho/menu-cabecalho.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-cabecalho',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-cabecalho.component.html',
+  styleUrl: './menu-cabecalho.component.css'
+})
+export class MenuCabecalhoComponent {
+
+}

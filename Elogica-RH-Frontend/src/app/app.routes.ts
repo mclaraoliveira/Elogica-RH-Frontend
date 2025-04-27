071555608b83bf62eb2bd13daac79186706092e6
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-27 10:33:31 -0300

    fix> ajustar vizualição da barra lateral header conteudo

--- Elogica-RH-Frontend/src/app/app.routes.ts
+++ Elogica-RH-Frontend/src/app/app.routes.ts
@@ -1,22 +1,22 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
+
 // import { InicialComponent } from './pages/inicial/inicial.component';
-=======
+
 import { InicialComponent } from './pages/inicial/inicial.component';
->>>>>>> main
+
 // import { ItensDeMenuComponent } from './pages/itens-de-menu/itens-de-menu.component';
 // import { CargosComponent } from './pages/cargos/cargos.component';
 // import { FuncionariosComponent } from './pages/funcionarios/funcionarios.component';
 // import { SetoresComponent } from './pages/setores/setores.component';
-<<<<<<< HEAD
+
 import { HorariosComponent } from './pages/horarios/horarios.component';
 // import { FeriasComponent } from './pages/ferias/ferias.component';
 // import { PaginaNaoEncontradaComponent } from './pages/pagina-nao-encontrada/pagina-nao-encontrada.component';
-=======
+
 // import { HorariosComponent } from './pages/horarios/horarios.component';
 // import { FeriasComponent } from './pages/ferias/ferias.component';
 import { PaginaNaoEncontradaComponent } from './pages/pagina-nao-encontrada/pagina-nao-encontrada.component';
->>>>>>> main
+
 
 export const routes: Routes = [
   {
@@ -24,19 +24,19 @@ export const routes: Routes = [
     redirectTo: '/inicial',
     pathMatch: 'full'
   },
-<<<<<<< HEAD
+
   // {
   //   path: 'inicial',
   //   component: InicialComponent,
   //   title: 'Página Inicial'
   // },
-=======
-  {
-    path: 'inicial',
-    component: InicialComponent,
-    title: 'Página Inicial'
-  },
->>>>>>> main
+
+  // {
+  //   path: 'inicial',
+  //   component: InicialComponent,
+  //   title: 'Página Inicial'
+  // },
+
   // {
   //   path: 'itens-de-menu',
   //   component: ItensDeMenuComponent,
@@ -57,35 +57,35 @@ export const routes: Routes = [
   //   component: SetoresComponent,
   //   title: 'Setores'
   // },
-<<<<<<< HEAD
+
   {
     path: 'horarios',
     component: HorariosComponent,
     title: 'Horários'
   },
-=======
+
   // {
   //   path: 'horarios',
   //   component: HorariosComponent,
   //   title: 'Horários'
   // },
->>>>>>> main
+
   // {
   //   path: 'ferias',
   //   component: FeriasComponent,
   //   title: 'Férias'
   // },
-<<<<<<< HEAD
+
   // {
   //   path: '**',
   //   component: PaginaNaoEncontradaComponent,
   //   title: 'Página não encontrada'
   // }
-=======
-  {
-    path: '**',
-    component: PaginaNaoEncontradaComponent,
-    title: 'Página não encontrada'
-  }
->>>>>>> main
+
+//   {
+//     path: '**',
+//     component: PaginaNaoEncontradaComponent,
+//     title: 'Página não encontrada'
+//   }
+
 ];

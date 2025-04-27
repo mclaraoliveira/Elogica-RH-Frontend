e3436d7631282761c0fac4d14be77ee58a7358b0
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-27 12:13:10 -0300

    feat: adicionar metodos e ajustar html

--- Elogica-RH-Frontend/src/app/pages/horarios/horarios.component.ts
+++ Elogica-RH-Frontend/src/app/pages/horarios/horarios.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { HorariosService } from '../../services/horarios.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-horarios',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './horarios.component.html',
   styleUrl: './horarios.component.css'
 })
 export class HorariosComponent {
 
-  
+  horarios: any[] = [];
+
+  constructor(private horariosService: HorariosService) { }
+
+  ngOnInit(): void{
+    this.listarHorarios();
+  }
+
+
+  listarHorarios():void{
+    this.horariosService.getHorarios().subscribe(data =>{
+      this.horarios = data;
+    });
+  }
+  adicionarHorarios(novoHorario: any):void {
+    this.horariosService.adicionarHorario(novoHorario).subscribe(data =>{
+      this.horarios.push(data);
+    })
+  }
 }

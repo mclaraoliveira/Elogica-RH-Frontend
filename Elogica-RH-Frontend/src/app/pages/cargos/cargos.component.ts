9eb380ffd5241ff85d02b0b101be5dc8e9221799
Vanvrs <vanessrodrigues@hotmail.com>
2025-04-26 21:51:07 -0300

    build: Criar paginas de cargos

---
+++ Elogica-RH-Frontend/src/app/pages/cargos/cargos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cargos',
+  standalone: true,
+  imports: [],
+  templateUrl: './cargos.component.html',
+  styleUrl: './cargos.component.css'
+})
+export class CargosComponent {
+
+}

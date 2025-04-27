4fd202840d7b9bb7a1831afdc2405aaa73dacd4b
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-27 11:30:10 -0300

    feat: criar services e adicionar url api

--- Elogica-RH-Frontend/src/app/pages/horarios/horarios.component.ts
+++ Elogica-RH-Frontend/src/app/pages/horarios/horarios.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-horarios',
   standalone: true,
-  imports: [HorariosComponent],
+  imports: [],
   templateUrl: './horarios.component.html',
   styleUrl: './horarios.component.css'
 })
 export class HorariosComponent {
 
+  
 }
---
+++ Elogica-RH-Frontend/src/app/services/horarios.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HorariosService {
+
+private apiUrl = "https://localhost:7050"
+
+  constructor() { }
+}

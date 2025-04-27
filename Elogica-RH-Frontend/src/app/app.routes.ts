f34f7d2abb32654377f4a4358d3fefddc0b53081
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-27 03:16:14 -0300

    fix: ajustar rotas

--- Elogica-RH-Frontend/src/app/app.routes.ts
+++ Elogica-RH-Frontend/src/app/app.routes.ts
@@ -2,11 +2,7 @@ import { Routes } from '@angular/router';
 import { InicialComponent } from './pages/inicial/inicial.component';
 // import { ItensDeMenuComponent } from './pages/itens-de-menu/itens-de-menu.component';
 // import { CargosComponent } from './pages/cargos/cargos.component';
-<<<<<<< HEAD
-import { FuncionariosComponent } from './pages/funcionarios/funcionarios.component';
-=======
 // import { FuncionariosComponent } from './pages/funcionarios/funcionarios.component';
->>>>>>> main
 // import { SetoresComponent } from './pages/setores/setores.component';
 // import { HorariosComponent } from './pages/horarios/horarios.component';
 // import { FeriasComponent } from './pages/ferias/ferias.component';
@@ -21,37 +17,16 @@ export const routes: Routes = [
   {
     path: 'inicial',
     component: InicialComponent,
-<<<<<<< HEAD
     title: 'Página Inicial',
-=======
-    title: 'Página Inicial'
->>>>>>> main
   },
   // {
   //   path: 'itens-de-menu',
   //   component: ItensDeMenuComponent,
-<<<<<<< HEAD
-  //   title: 'Itens de Menu',
-=======
   //   title: 'Itens de Menu'
->>>>>>> main
   // },
   // {
   //   path: 'cargos',
   //   component: CargosComponent,
-<<<<<<< HEAD
-  //   title: 'Cargos',
-  // },
-  {
-    path: 'funcionarios',
-    component: FuncionariosComponent,
-    title: 'Funcionários',
-  },
-  // {
-  //   path: 'setores',
-  //   component: SetoresComponent,
-  //   title: 'Setores',
-=======
   //   title: 'Cargos'
   // },
   // {
@@ -63,25 +38,16 @@ export const routes: Routes = [
   //   path: 'setores',
   //   component: SetoresComponent,
   //   title: 'Setores'
->>>>>>> main
   // },
   // {
   //   path: 'horarios',
   //   component: HorariosComponent,
-<<<<<<< HEAD
-  //   title: 'Horários',
-=======
   //   title: 'Horários'
->>>>>>> main
   // },
   // {
   //   path: 'ferias',
   //   component: FeriasComponent,
-<<<<<<< HEAD
-  //   title: 'Férias',
-=======
   //   title: 'Férias'
->>>>>>> main
   // },
   {
     path: '**',

7fbf8d84662328fb8b9b6f7026e8c19de04f6a80
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-27 15:59:11 -0300

    build: criar componente setores

---
+++ Elogica-RH-Frontend/src/app/pages/setores/setores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setores',
+  standalone: true,
+  imports: [],
+  templateUrl: './setores.component.html',
+  styleUrl: './setores.component.css'
+})
+export class SetoresComponent {
+
+}

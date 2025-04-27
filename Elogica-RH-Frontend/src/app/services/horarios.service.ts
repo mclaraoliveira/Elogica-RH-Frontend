0fae9b2c49e25d7162f6e4d0431744f27f6f824d
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-27 11:33:34 -0300

    feat:inserir no service

--- Elogica-RH-Frontend/src/app/services/horarios.service.ts
+++ Elogica-RH-Frontend/src/app/services/horarios.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,13 @@ export class HorariosService {
 
 private apiUrl = "https://localhost:7050"
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getHorarios():Observable<any[]>{
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  adicionarHorario(horario: any):Observable<any>{
+    return this.http.post<any>(this.apiUrl, horario);
+  }
 }

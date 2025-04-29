f86953fd6bd533bde931dd3cdf7f32e237e7f2a7
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-29 13:25:36 -0300

    feat: ajustar menu lateral ts

--- Elogica-RH-Frontend/src/app/shared/components/menu-lateral/menu-lateral.component.ts
+++ Elogica-RH-Frontend/src/app/shared/components/menu-lateral/menu-lateral.component.ts
@@ -1,42 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuLateralService, Menu } from '../../services/menu-lateral.service';
+import { MenuLateralService } from '../../services/menu-lateral.service';
 import { CommonModule } from '@angular/common';
+import { Menu } from '../../interfaces/menu';
 
 @Component({
   selector: 'app-menu-lateral',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './menu-lateral.component.html',
-  styleUrl: './menu-lateral.component.css'
+  styleUrl: './menu-lateral.component.css',
 })
 export class MenuLateralComponent implements OnInit {
+  menusPais: Menu[] = [];
+  menusFilhos: { [key: number]: Menu[] } = {};
+  menusAbertos: { [key: number]: boolean } = {}; // <<< Aqui controla abertos
 
-    menusPais: Menu[] = [];
-    menusFilhos: { [key: number]: Menu[] } = {};
-    menusAbertos: { [key: number]: boolean } = {}; // <<< Aqui controla abertos
+  constructor(private menuLateralService: MenuLateralService) {}
 
-    constructor(private menuLateralService: MenuLateralService) { }
+  ngOnInit(): void {
+    this.menuLateralService.listar().subscribe((menus) => {
+      this.menusPais = menus
+        .filter((menu) => menu.menuPaiId === 0)
+        .sort((a, b) => a.ordem - b.ordem);
 
-    ngOnInit(): void {
-      this.menuLateralService.listar().subscribe(menus => {
-        this.menusPais = menus.filter(menu => menu.menuPaiId === 0)
-                               .sort((a, b) => a.ordem - b.ordem);
+      menus
+        .filter((menu) => menu.menuPaiId !== 0)
+        .forEach((filho) => {
+          if (!this.menusFilhos[filho.menuPaiId]) {
+            this.menusFilhos[filho.menuPaiId] = [];
+          }
+          this.menusFilhos[filho.menuPaiId].push(filho);
+        });
 
-        menus.filter(menu => menu.menuPaiId !== 0)
-             .forEach(filho => {
-               if (!this.menusFilhos[filho.menuPaiId]) {
-                 this.menusFilhos[filho.menuPaiId] = [];
-               }
-               this.menusFilhos[filho.menuPaiId].push(filho);
-             });
-
-        for (let key in this.menusFilhos) {
-          this.menusFilhos[key] = this.menusFilhos[key].sort((a, b) => a.ordem - b.ordem);
-        }
-      });
-    }
-
-    toggleMenu(menuId: number) {
-      this.menusAbertos[menuId] = !this.menusAbertos[menuId];
-    }
+      for (let key in this.menusFilhos) {
+        this.menusFilhos[key] = this.menusFilhos[key].sort(
+          (a, b) => a.ordem - b.ordem
+        );
+      }
+    });
   }
+
+  toggleMenu(menuId: number) {
+    this.menusAbertos[menuId] = !this.menusAbertos[menuId];
+  }
+}

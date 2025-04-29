556694254764c96bbc14d7ac16b89aed1c0f3f05
Vanvrs <vanessrodrigues@hotmail.com>
2025-04-29 14:58:10 -0300

    fix: Implementar services e interfaces

--- Elogica-RH-Frontend/src/app/services/cargos/cargos.service.ts
+++ Elogica-RH-Frontend/src/app/services/cargos/cargos.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Cargo, CriarCargoDto, AtualizarCargoDto } from '../../shared/interfaces/cargo';
+import { map, Observable } from 'rxjs';
+import { Cargo} from '../../shared/interfaces/cargo';
 import { RetornoPaginado } from '../../shared/interfaces/retornoPaginado';
+import { SetoresService } from '../setores/setores.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CargosService {
+
+
   private readonly API = 'https://localhost:7050/cargos';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private setorService: SetoresService) { }
+
+  buscarCargoPorId(id: number): Observable<Cargo> {
+    return this.http.get<Cargo>(`${this.API}/${id}`);
+  };
 
   buscarCargosPaginado(pagina: number, quantidade: number): Observable<RetornoPaginado<Cargo>> {
-    return this.http.get<RetornoPaginado<Cargo>>(`${this.API}/${pagina}/${quantidade}`);
+    return this.http.get<any>(`${this.API}/${pagina}/${quantidade}`)
+      .pipe(map(resposta => resposta.data));
   }
 
-  adicionarCargo(cargo: CriarCargoDto): Observable<number> {
+  adicionarCargo(cargo: Cargo): Observable<number> {
     return this.http.post<number>(this.API, cargo);
   }
 
-  atualizarCargo(id: number, cargo: AtualizarCargoDto): Observable<Cargo> {
+  atualizarCargo(id: number, cargo: Cargo): Observable<Cargo> {
     return this.http.put<Cargo>(`${this.API}/${id}`, cargo);
   }
 

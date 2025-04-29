e1fee14b0bd1f97f28f5efb6b7c308eaaa17dfff
Vanvrs <vanessrodrigues@hotmail.com>
2025-04-29 07:07:44 -0300

    fix: Implmentar o cargo service

--- Elogica-RH-Frontend/src/app/services/cargos.service.ts
+++ Elogica-RH-Frontend/src/app/services/cargos.service.ts
@@ -1,96 +1,33 @@
-// src/app/services/cargo.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
-import { environment } from '../../environments/environments';
-import {
-  Cargo,
-  CargoDto,
-  AtualizarCargoDto,
-} from '../shared/interfaces/cargo';
-import { ModalService } from '../../app/shared/services/modal.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cargo, CriarCargoDto, AtualizarCargoDto } from '../shared/interfaces/cargo';
 import { RetornoPaginado } from '../shared/interfaces/retornoPaginado';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CargoService {
-  private apiUrl = `${environment.apiUrl}/cargos`;
+export class CargosService {
+  private readonly API = 'https://localhost:7050/cargos';
 
-  constructor(
-    private http: HttpClient,
-    private modalService: ModalService
-  ) { }
+  constructor(private http: HttpClient) { }
 
-  // Listar cargos paginados
-  listarCargosPaginado(pagina: number, quantidade: number): Observable<RetornoPaginado<Cargo>> {
-    const params = new HttpParams()
-      .set('pagina', pagina.toString())
-      .set('quantidade', quantidade.toString());
-
-    return this.http.get<RetornoPaginado<Cargo>>(this.apiUrl, { params })
-      .pipe(catchError(this.handleError.bind(this)));
+  buscarCargosPaginado(pagina: number, quantidade: number): Observable<RetornoPaginado<Cargo>> {
+    return this.http.get<RetornoPaginado<Cargo>>(`${this.API}/${pagina}/${quantidade}`);
   }
 
-  // Listar todos os cargos
-  listarCargos(): Observable<Cargo[]> {
-    return this.http.get<Cargo[]>(this.apiUrl)
-      .pipe(catchError(this.handleError.bind(this)));
+  adicionarCargo(cargo: CriarCargoDto): Observable<number> {
+    return this.http.post<number>(this.API, cargo);
   }
 
-  // Obter cargo por ID
-  obterCargoPorId(id: number): Observable<Cargo> {
-    return this.http.get<Cargo>(`${this.apiUrl}/${id}`)
-      .pipe(catchError(this.handleError.bind(this)));
+  atualizarCargo(id: number, cargo: AtualizarCargoDto): Observable<Cargo> {
+    return this.http.put<Cargo>(`${this.API}/${id}`, cargo);
   }
 
-  // Adicionar novo cargo
-  adicionarCargo(cargoDto: CargoDto): Observable<number> {
-    return this.http.post<number>(this.apiUrl, cargoDto)
-      .pipe(catchError(this.handleError.bind(this)));
-  }
-
-  // Atualizar cargo existente
-  atualizarCargo(id: number, cargoDto: AtualizarCargoDto): Observable<boolean> {
-    return this.http.put<boolean>(`${this.apiUrl}/${id}`, cargoDto)
-      .pipe(catchError(this.handleError.bind(this)));
-  }
-
-  // Excluir cargo
   excluirCargo(id: number): Observable<boolean> {
-    return this.http.delete<boolean>(`${this.apiUrl}/${id}`)
-      .pipe(catchError(this.handleError.bind(this)));
+    return this.http.delete<boolean>(`${this.API}/${id}`);
   }
-
-  // Tratamento de erros
-  private handleError(error: HttpErrorResponse) {
-    let errorMessage = 'Ocorreu um erro desconhecido';
-
-    if (error.error instanceof ErrorEvent) {
-      // Erro do lado do cliente
-      errorMessage = `Erro: ${error.error.message}`;
-    } else {
-      // Erro do lado do servidor
-      if (error.status === 0) {
-        errorMessage = 'Não foi possível conectar ao servidor';
-      } else if (error.status === 400) {
-        // Tratar erros de validação
-        if (error.error?.errors) {
-          const errors = error.error.errors;
-          errorMessage = Object.values(errors).join('\n');
-        } else if (error.error?.message) {
-          errorMessage = error.error.message;
-        } else {
-          errorMessage = 'Dados inválidos enviados';
-        }
-      } else if (error.status === 404) {
-        errorMessage = 'Recurso não encontrado';
-      } else if (error.status >= 500) {
-        errorMessage = 'Erro interno do servidor';
-      }
-    }
-
-    this.modalService.erro('Erro na API', errorMessage);
-    return throwError(() => new Error(errorMessage));
+  vincularSetores(idCargo: number, setoresIds: number[]): Observable<Cargo> {
+    return this.http.put<Cargo>(`${this.API}/${idCargo}/setores`, { setoresIds });
   }
 }
